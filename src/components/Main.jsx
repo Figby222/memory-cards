0b7af339deb8b046a0ef41394cf1c8d93db81fb5
Figby222 <figby222@outlook.com>
2024-06-05 01:27:01 -0700

    Add function getShuffledArray()

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,17 @@
 import { useState } from 'react';
 
+function getShuffledArray(array) {
+    const newArray = [];
+    while(newArray.length < array.length) {
+        const index = Math.floor(Math.random() * array.length);
+        if (!(newArray.includes(array[index]))) {
+            newArray.push(array[index]);
+        }
+    }
+
+    return newArray;
+}
+
 function Main() {
     let keys = [ "asdasd", "paiaufghisf", ";oiuhsadgiuagdh", "iauefiuaifu"];
     const [ selectedKeys, setSelectedKeys ] = useState([]);
