fae375d2ce670d6cdbac8fa9a7430db7f403bdff
Figby222 <figby222@outlook.com>
2024-06-07 09:36:55 -0700

    root: Remove padding & margin from body

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header.jsx';
 import Main from './components/Main.jsx';
+import './styles/root.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
